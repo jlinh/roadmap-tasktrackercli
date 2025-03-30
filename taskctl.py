9c58331787a980d74b3afec82d1fdf1a176aa645
jlinh <hujialin1995@gmail.com>
2025-03-30 12:47:07 +0000

    Task initialization and validation functions

--- taskctl.py
+++ taskctl.py
@@ -1,14 +1,18 @@
 import sys
 import os
 import json
+from datetime import datetime
 from argparse import ArgumentParser
 from datetime import datetime
 from tabulate import tabulate
 
 
 def main():
-    pass
-    tasks = init_task_json()
+    try:
+        tasks = init_task_json()
+    except:
+        print("Error initializing task JSON file.")
+        raise
     print(tasks)
 
 
@@ -36,8 +40,41 @@ def init_task_json():
         with open('tasks.json', 'r') as f:
             tasks = json.load(f)
             if not isinstance(tasks, list):
-                raise ValueError("Invalid task data format")
+                raise ValueError("Invalid task list data format")
+            isValidTasks = [is_valid_task(task) for task in tasks]
+            if not all(isValidTasks):
+                raise ValueError("Invalid task data format found in the list")
+            task_ids = [task['id'] for task in tasks]
+            if len(task_ids) != len(set(task_ids)):
+                raise ValueError("Duplicate task IDs found")
+            if sorted(task_ids) != task_ids:
+                raise ValueError("Task IDs are not sorted")  # Could be warning
     return tasks
+
+
+def is_valid_task(task):
+    if not isinstance(task, dict):
+        return False
+    else:
+        required_keys = ['id', 'description',
+                         'status', 'createdAt', 'updatedAt']
+        contains_required_keys = all(
+            [key in required_keys for key in task.keys()])
+        if not contains_required_keys:
+            return False
+        try:
+            datetime.fromisoformat(task['createdAt'])
+            datetime.fromisoformat(task['updatedAt'])
+        except:
+            raise
+        return True
+
+
+def parse_obj(taskDict):
+    if isinstance(taskDict, dict):
+        return {k: parse_obj(v) for k, v in taskDict.items()}
+    else:
+        raise ValueError("Invalid task data format")
 
 
 if __name__ == '__main__':
